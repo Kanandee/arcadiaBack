6b7a41b9e6729611b9544c9d33d4b43523089106
Kanandee <lauritapeba@hotmail.com>
2023-02-06 13:51:14 +0100

    feat: added database config

---
+++ config/database.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+mongoose.set("strictQuery", true);
+
+const db = () =>
+   mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => {
+         console.log("Connection stablished on: ", process.env.MONGO_URI);
+      })
+      .catch((error) => {
+         console.log("Error connecting to MongoDB", error);
+      });
+
+export default db;
